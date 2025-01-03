fe2ced4328dac5d34b556d6e69acd0b57ee2851d
Daniel Reilly <reillyd@nucrypt.net>
2025-01-03 14:12:22 -0600

    got DAC3 dacs to output

---
+++ projects/daq3/zcu106/setup.sh
@@ -0,0 +1,14 @@
+echo running setup.sh, which is set up for windows
+echo If running in linux, this must change.
+export PATH=${PATH}:/cygdrive/C/Xilinx/Vitis/2023.2/bin
+export PATH=${PATH}:/cygdrive/C/Xilinx/Vitis/2023.2/gnu/microblaze/nt/bin
+export PATH=${PATH}:/cygdrive/C/Xilinx/Vitis/2023.2/gnu/armr5/nt/bin
+# export PATH=${PATH}:/cygdrive/C/Xilinx/Vitis/2023.2/gnu/microblaze/linux_toolchain/nt64_be/bin
+export PATH=${PATH}:/cygdrive/C/Xilinx/Vitis/2023.2/gnu/microblaze/linux_toolchain/nt64_le/bin
+export PATH=${PATH}:/cygdrive/C/Xilinx/Vitis/2023.2/gnu/aarch32/nt/gcc-arm-none-eabi/bin
+# export PATH=${PATH}:/cygdrive/C/intelFPGA_pro/2x.x/quartus/bin
+export PATH=/cygdrive/C/Xilinx/Vivado/2023.2/bin:${PATH}
+export PATH=/cygdrive/C/Xilinx/Vitis_HLS/2023.2/bin:${PATH}
+export PATH=/cygdrive/C/Xilinx/Vitis/2023.2/bin:${PATH}
+echo ${PATH}
+
